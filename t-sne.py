11a2c61ac00e84943fa89b7f94432f729a0bea8d
rjavadi <roya.javadi26@gmail.com>
2018-10-14 19:20:38 +0330

    refactoring and commenting

--- t-sne.py
+++ t-sne.py
@@ -1,16 +1,11 @@
 import os
 import tensorflow as tf
-from tensorflow.contrib.tensorboard.plugins import projector
 import numpy as np
 import pandas as pd
 import pickle
-import time
-from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 from ggplot import ggplot, aes, geom_point, ggtitle
-import plotly
-import plotly.graph_objs as go
 
 
 def open_pickle(pickle_file):
@@ -30,21 +25,33 @@ assert len(embedding_shape) == 1
 
 embedding_data = [item[3] for item in embeddings['caption_embedding_tuples']]
 model_id = [item[2] for item in embeddings['caption_embedding_tuples']]
-print("df shape: ", np.shape(embedding_data))
-print("labels shape: ", np.shape(model_id))
+print("embedding shape: ", np.shape(embedding_data))
+print("model_id shape: ", np.shape(model_id))
 
 feat_cols = ['col' + str(i) for i in range(np.shape(embedding_data)[1])]
 df = pd.DataFrame(embedding_data, columns=feat_cols)
+
+
+"""Reading categories to use it as a label for coloring/separating data on chart"""
 labels_df = pd.read_csv(os.path.join("F:/", "DL-code", "text2shape-data", "captions.csv"), usecols=['modelId', 'category'])
+
+""" reformat it as a list of dicts """
 captions_dict = labels_df.to_dict(orient='records')
+
+""" making a dictionary with format: #model_id: Category """
 labels_dict = {}
 for i in range(len(captions_dict)):
     if captions_dict[i]['modelId'] in model_id:
         labels_dict[captions_dict[i]['modelId']] = captions_dict[i]['category']
+
+
+# the order of labels and models is preserved
 df['label'] = [labels_dict[id] for id in model_id]
 
 print('Size of the dataframe: {}'.format(df.shape))
 
+
+# Apply PCA transform
 pca = PCA(n_components=2)
 pca_result = pca.fit_transform(df[feat_cols].values)
 
@@ -60,10 +67,8 @@ chart = ggplot(df_pca, aes(x = 'pca_1', y = 'pca_2', color='label')) \
 chart.show()
 
 
-time_start = time.time()
 tsne = TSNE(n_components=2, verbose=1, perplexity=40, n_iter=300)
 tsne_results = tsne.fit_transform(df[feat_cols].values)
-print('t-SNE done! Time elapsed: {} seconds'.format(time.time()-time_start))
 
 df_tsne = df.copy()
 df_tsne['x_tsne'] = tsne_results[:, 0]
@@ -75,7 +80,6 @@ chart = ggplot( df_tsne, aes(x = 'x_tsne', y = 'y_tsne', color='label') ) \
 chart.show()
 
 
-# embedding_var = tf.Variable(data, name=NAME_TO_VISUALISE_VARIABLE)
 summary_writer = tf.summary.FileWriter(LOG_DIR)
 
 
